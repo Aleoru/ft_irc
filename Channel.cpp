61890e19dac2ee0e3f192a9ee4e87918ed5211a1
fgalan-r <tremerin13@gmail.com>
2024-04-05 20:35:47 +0200

    Channel list

--- Channel.cpp
+++ Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: fgalan-r <fgalan-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 20:19:31 by fgalan-r          #+#    #+#             */
-/*   Updated: 2024/04/05 20:30:34 by fgalan-r         ###   ########.fr       */
+/*   Updated: 2024/04/05 20:35:29 by fgalan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ Channel::~Channel()
 
 std::vector<int> Channel::channelListUsers()
 {
-    
+    std::vector<int> channelUsers;
+
+    return (channelUsers);
 }
